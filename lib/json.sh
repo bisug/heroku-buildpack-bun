0ef6b34a140a73d34a1da8aa922a0a21be743898
Bisu Ghalan <bisu.ghlan@gmail.com>
2026-08-18 13:43:50 +0545

    Clean up compile script and ignore build artifact

--- lib/json.sh
+++ lib/json.sh
@@ -2,20 +2,6 @@
 # lib/json.sh - JSON helper functions (sourced by bin/compile and bin/release)
 # Based on: https://github.com/heroku/heroku-buildpack-nodejs/blob/main/lib/json.sh
 
-read_json() {
-  local file="$1"
-  local key="$2"
-
-  if test -f "$file"; then
-    # -c = print on only one line
-    # -M = strip any color
-    # --raw-output = write string results directly to stdout (no JSON quoting)
-    jq -c -M --raw-output "$key // \"\"" "$file" || return 1
-  else
-    echo ""
-  fi
-}
-
 has_script() {
   local file="$1"
   local key="$2"
